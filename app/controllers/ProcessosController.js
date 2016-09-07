d07dcc4cd459e8f62f312f952a33887a0687bd10
renanlemos <renan-m-lemos@hotmail.com>
2016-09-06 23:53:44 -0300

    it

--- app/controllers/ProcessosController.js
+++ app/controllers/ProcessosController.js
@@ -11,21 +11,20 @@ $app.controller("ProcessosController",function($scope,$timeout,$rootScope,Simula
     **/ 
 	var rowProcesso = function(){
 
-		var processos = ProcessosService.getProcessos();
+		var processos      = ProcessosService.getProcessos();
 
 		if(processos.count == 0){
 			/** Cria um processos **/
 			ProcessosService.createProcesso();
-
 		}else{
 
 			/** Sorteia um número entre 100 e 300 **/
 			var sorteio = SimuladorService.getRandomInt(1,100);
 
-			if(sorteio <= 10){
+			if(sorteio <= 10 && processos.count < $scope.NumeroProcessos){
 				ProcessosService.createProcesso();
 			}else{
-
+				
 			}
 		}
 
@@ -42,17 +41,21 @@ $app.controller("ProcessosController",function($scope,$timeout,$rootScope,Simula
     **/ 
 	var processos = function(pid){
 
-		var params          = SimuladorService.getParams();
-		var NumeroProcessos = params.processos;
-		var TempoCiclo      = params.tempo_ciclos * 1000;
+		if($rootScope.simuladorStatus == false){
+			return false;
+		}
 
-		if($scope.destruidos <= NumeroProcessos){
+		var params             = SimuladorService.getParams();
+		$scope.NumeroProcessos = params.processos;
+		$scope.TempoCiclo      = params.tempo_ciclos * 1000;
+
+		if($scope.destruidos <= $scope.NumeroProcessos){
 
 			rowProcesso();
 
 			$timeout(function(){
 				processos();
-			},TempoCiclo);
+			},$scope.TempoCiclo);
 
 		}
 
@@ -62,7 +65,7 @@ $app.controller("ProcessosController",function($scope,$timeout,$rootScope,Simula
 
 		localStorage.clear();
 		$rootScope.simuladorStatus = false;
-		$scope.processos           =  {data:[],count:0};;
+		$scope.processos           =  {data:[],count:0};
 	}
 	/**
 	*
@@ -77,7 +80,6 @@ $app.controller("ProcessosController",function($scope,$timeout,$rootScope,Simula
 		processos();
 		$scope.processos      = ProcessosService.getProcessos();
 
-		console.log($scope.processos);
 	
 	}
 	init(); 
